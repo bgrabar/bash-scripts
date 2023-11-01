8a8525a6d05af20e7b3479092cd18a38471dd623
bgrabar <bbgrbr@gmail.com>
2023-10-31 20:18:46 -0400

    add comments

--- rm-DS_Store.sh
+++ rm-DS_Store.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+# remove .DS_Store from current directory and subdirectories,
+# down to the fourth level of subdirectories
 rm .DS_Store */.DS_Store */*/.DS_Store */*/*/.DS_Store */*/*/*/.DS_Store
--- tree.sh
+++ tree.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+# prints the directory structure in the form of a tree
 find . -print | sed -e 's;[^/]*/;|____;g;s;____|; |;g'
