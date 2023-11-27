c50c6003365872eaa1a6dc32cabfbcc79878e26f
bgrabar <bbgrbr@gmail.com>
2023-11-27 14:26:40 -0500

    update placeholder text

--- mdb/run.standalone.sh
+++ mdb/run.standalone.sh
@@ -1,10 +1,8 @@
-# replace ZZZZZZ with home directory name
-
+# replace <HOMEDIR> with home directory name
 
 #!/bin/bash
 
-mongod --port 35017 --config /Users/ZZZZZZ/myrepo/scripts/mdb/config-files/standalone.conf
+mongod --port 35017 --config /Users/<HOMEDIR>/repos/bash-scripts/mdb/config-files/standalone.conf
 
-
-# To run the compiled 'mongod' in /Users/ZZZZZZ/mongo:
-# /Users/ZZZZZZ/mongo/mongod --port 35017 --config /Users/ZZZZZZ/myrepo/scripts/mdb/config-files/standalone.conf
+# To run the compiled 'mongod' in /Users/<HOMEDIR>/mongo:
+# /Users/<HOMEDIR>/mongo/mongod --port 35017 --config /Users/<HOMEDIR>/repos/bash-scripts/mdb/config-files/standalone.conf
