3bc671bea2ddcb157ea4fe24fdf73b6db5e9ea07
bgrabar <bbgrbr@gmail.com>
2024-01-19 13:30:07 -0500

    use _ in filename

--- rm_DS_Store.sh
+++ rm_DS_Store.sh
@@ -2,4 +2,5 @@
 
 # remove .DS_Store from current directory and subdirectories,
 # down to the fourth level of subdirectories
+
 rm .DS_Store */.DS_Store */*/.DS_Store */*/*/.DS_Store */*/*/*/.DS_Store
