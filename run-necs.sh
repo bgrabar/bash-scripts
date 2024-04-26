c03a645155785dc1be8dd8ac5332d89236c06b87
bgrabar <bbgrbr@gmail.com>
2024-04-26 09:39:01 -0400

    add script

---
+++ run-necs.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+open /Applications/Barracuda\ VPN\ Client.app/
+
+sleep 5 # pauses for 5 seconds
+
+open /Applications/Microsoft\ Remote\ Desktop.app/
+
+sleep 10 # pauses
+
+open /Applications/Android\ Studio.app/
