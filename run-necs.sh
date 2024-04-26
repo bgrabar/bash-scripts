4dcd5ac8d4634271a4e4cfc53039b680a9f013a2
bgrabar <bbgrbr@gmail.com>
2024-04-26 12:12:57 -0400

    update sleep values

--- run-necs.sh
+++ run-necs.sh
@@ -2,10 +2,10 @@
 
 open /Applications/Barracuda\ VPN\ Client.app/
 
-sleep 5 # pauses for 5 seconds
+sleep 8 # pauses for 5 seconds
 
 open /Applications/Microsoft\ Remote\ Desktop.app/
 
-sleep 10 # pauses
+sleep 15 # pauses
 
 open /Applications/Android\ Studio.app/
